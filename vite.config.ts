e3e919491ff1264bde8c473c3241b8afa2b4882f
tanitahseen-product <tani.tahseen@gmail.com>
2026-02-11 11:44:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base:'/product-story-showcase/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
